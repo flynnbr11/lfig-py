6a6083027879146fc8415c974a0f241d923b2a4f
brian-phasecraft <brian@phasecraft.io>
2023-09-06 14:50:23 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='lfig',
-    version='0.1.2',
+    name="lfig",
+    version="0.1.3",
     # package_dir={'': 'src'},
-    packages=find_packages(exclude=['tests*']),
-    license='MIT',
-    description='Latex figures from matplotlib',
-    long_description=open('README.md').read(),
-    install_requires=['numpy', 'matplotlib'],
+    packages=find_packages(exclude=["tests*"]),
+    license="MIT",
+    description="Latex figures from matplotlib",
+    long_description=open("README.md").read(),
+    install_requires=["numpy", "matplotlib"],
     # url='https://github.com/BillMills/python-package-example',
-    author='Brian Flynn',
-    author_email='flynnbr@tcd.ie'
+    url="https://github.com/flynnbr11/lfig-py",
+    author="Brian Flynn",
+    author_email="flynnbr@tcd.ie",
 )
